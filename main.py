e6be414ca23b9f40ef4cc3e358388ac15789b2db
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-05-04 00:33:55 +0800

    Editing DropDownWidget

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from kivy.app import app
+from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.lang import Builder
 
+Builder.load_file('memory.kv')
+
+Window.size = (600,400)
 
 class DropdownWidget(Widget):
     pass
@@ -18,10 +24,10 @@ class DeckButtonsWidget(Widget):
 class FrontLayout(BoxLayout):
     pass
 
-class MemorizationApp(App):
+class MemoryApp(App):
     def build(self):
         frontpage = FrontLayout()
         return frontpage
 
 if __name__=='__main__':
-    MemorizationApp().run()
+    MemoryApp().run()
