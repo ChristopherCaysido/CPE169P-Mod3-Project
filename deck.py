cdc1d15df3c2de0cd68906ba1d8cbbf1c3664b8f
Justin Rupert Reyes <justinrupert.reyes@gmail.com>
2021-05-05 01:08:14 +0900

    Modify deck.py

--- deck.py
+++ deck.py
@@ -1,13 +1,26 @@
-
+import random
 
 class Deck:
-    def __init__(self, name=None, items=None):
+    def __init__(self, name='', items={}):
         self.name = name
         self.items = items 
 
-    def draw(self):
+    def draw_item(self):
+        '''Draws an item from deck'''
+        return random.choice(self.items.items(), 
+                            weights = [item.weight for item in self.items.items()])
+    
+    def draw_answer(self, n)
+        '''Draws an n number of answers/s from the deck'''
         pass
 
+    def update_weights(self, items):
+        '''Updates the weights of the items in the deck'''
+        for key in items: 
+            self.items[key].weight = items[key]
+
 class Item:
-    def __init__(self):
-        self.text
+    def __init__(self, text = '', answers = [], weight = 0):
+        self.text = text
+        self.answers = answers
+        self.weight = weight
